cc6e26ffe4c2cf48fc21a88b8894ddf85044cc06
Angga Aditya <90344608+anggadityaa@users.noreply.github.com>
2023-03-29 16:04:57 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function ubahBentuk() {
+  btnAbout.style.transition = "0.5s";
+  btnAbout.style.width = "120px";
+  btnAbout.style.height = "50px";
+  //   btnAbout.style.borderRadius = "8px";
+  btnAbout.style.fontSize = "20px";
+}
+function ubahBentuk2() {
+  btnAbout.style.width = "97px";
+  btnAbout.style.height = "38px";
+  btnAbout.style.transition = "0.5s";
+  btnAbout.style.borderRadius = "5px";
+  btnAbout.style.fontSize = "16px";
+}
